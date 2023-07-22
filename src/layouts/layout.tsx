30a44a53aa1243020a9c3fa6a9060cb4f303c6cb
backm <yousseffmohamedd22@gmail.com>
2023-07-23 01:38:00 +0300

    fix error

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -41,7 +41,7 @@ export const DashboardLayout = (props: { children: any; }) => {
 
     return (
         <>
-            <TopNav onNavOpen={() => setOpenNav(true)} />
+            <TopNav />
             <LayoutRoot>
                 <LayoutContainer>
                     {children}
