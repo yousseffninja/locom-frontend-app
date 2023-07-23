ab9e25930d02db2278d3c17315fcbfe6f8612950
backm <yousseffmohamedd22@gmail.com>
2023-07-23 14:19:58 +0300

    Fix error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,17 @@
-import {DashboardLayout} from "@/layouts/layout";
 import {TopNav} from "@/layouts/top-nav";
 import React, { useState, useEffect } from "react";
 import { useTheme } from '@mui/material/styles';
 import {
     Stack,
     Box,
-    Typography,
-    TextField, InputAdornment,Button,OutlinedInput
 } from "@mui/material";
 
 import { FirstSection } from "@/components/home-page/first-section";
 
 export const Page = () => {
     const theme = useTheme();
-    const [deviceWidth, setDeviceWidth] = useState(window.innerWidth);
-    const [deviceHeight, setDeviceHeight] = useState(window.innerHeight);
+    const [deviceWidth, setDeviceWidth] = useState(1920);
+    const [deviceHeight, setDeviceHeight] = useState(1080);
 
     const handleResize = () => {
         setDeviceWidth(window.innerWidth);
@@ -22,8 +19,14 @@ export const Page = () => {
     };
 
     useEffect(() => {
+        // Add event listener to update dimensions on window resize
         window.addEventListener('resize', handleResize);
 
+        // Set initial dimensions on component mount
+        setDeviceWidth(window.innerWidth);
+        setDeviceHeight(window.innerHeight);
+
+        // Clean up the event listener when the component is unmounted
         return () => {
             window.removeEventListener('resize', handleResize);
         };
